82927e433e94b92d1af6ef094ed24656d0af98ef
Suvam Basak <suvambasak2008@gmail.com>
2022-08-18 22:41:50 +0530

    Q1

--- assignment_1/Q1/adder_test.v
+++ assignment_1/Q1/adder_test.v
@@ -22,13 +22,20 @@ module adder_test;
         $dumpvars(0,adder_test);
 
         data0 = 8'b00000000;
-        data1 = 8'b00000000;
+        // data1 = 8'b00000000;
+
+        // data0 = 8'b11111111;
+        data1 = 8'b11111111;
 
         mode = 1'b1;
     end
 
     always #5 data0=data0+1'b1;
-    always #10 data1=data1+1'b1;
+    // always #5 data0=data0-1'b1;
+
+    // always #10 data1=data1+1'b1;
+    always #10 data1=data1-1'b1;
+    
     always #35 mode=mode+1'b1;
 
     initial $monitor($time," mode=%b  data0=%b, data1=%b | final_sum=%b, final_carry_out=%b, overflow=%b", mode, data0, data1, final_sum, final_carry_out, overflow);
